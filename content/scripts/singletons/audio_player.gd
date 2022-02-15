308626a2495b9d3c2fc872d93cc85d4cc614119d
JezSonic <karotomaszewskimusic@gmail.com>
2022-02-15 21:33:58 +0100

    change node getting method

--- content/scripts/singletons/audio_player.gd
+++ content/scripts/singletons/audio_player.gd
@@ -1,5 +1,10 @@
 extends Node
-
+# cache nodes
+onready var music_ending = $music_ending
+onready var music_full = $music_full
+onready var music_middle = $music_middle
+onready var music_intro = $music_intro
+onready var sfx = $sfx
 # song names, use them for play_song function
 enum Songs {
 	Hill = 0,
@@ -30,19 +35,19 @@ func play_song(song_idx:int, play_intro:bool=true, play_ending:bool=true, loop_m
 	if play_intro:
 		_current_song_queue.append("intro")
 		_intro_file_path = _random_file(Array(_song_data['song_intros']))
-		$music_intro.stream = load(_intro_file_path)
+		music_intro.stream = load(_intro_file_path)
 	if play_ending:
 		_current_song_queue.append("ending")
 		_ending_file_path = _random_file(Array(_song_data['song_endings']))
-		$music_ending.stream = load(_ending_file_path)
+		music_ending.stream = load(_ending_file_path)
 	_middle_file_path = _get_song_data(song_name)['song_middle']
-	$music_middle.stream = load(_random_file(Array(_middle_file_path)))
+	music_middle.stream = load(_random_file(Array(_middle_file_path)))
 	
 	#start playing by queue, for now we only chceck if music was marked to play with intro or not and play intro if it is needed.
 	if _current_song_queue.has("intro"):
-		$music_intro.play()
+		music_intro.play()
 	else:
-		$music_middle.play()
+		music_middle.play()
 # getting all information about song files and sfx
 func _get_song_data(song_name:String) -> Dictionary:
 	var _song_data = {}
@@ -67,65 +72,65 @@ func _random_file(file_array:Array) -> String:
 func _on_music_intro_finished() -> void:
 	print("Intro finished")
 	_current_song_queue.remove(_current_song_queue.find("intro")) #removing "intro"
-	$music_intro.stop()
-	$music_middle.play() #everything was preloaded previously, so for now we only call play method
+	music_intro.stop()
+	music_middle.play() #everything was preloaded previously, so for now we only call play method
 
 
 func _on_music_middle_finished() -> void:
 	if _middle_loop:
-		$music_middle.play()
+		music_middle.play()
 	else:
 		_current_song_queue.remove(_current_song_queue.find("middle")) #removing "intro"
 		# for now we check if we have to play ending, if so, ending will be played
 		if _current_song_queue.has("ending"):
-			$music_ending.play()
+			music_ending.play()
 		else:
 			_current_song_queue = [] #clear the song queue
 
 
 func _on_music_ending_finished() -> void:
 	_current_song_queue = [] #clear the song queue
-	$music_ending.stop() # just make sure ending won't loop
+	music_ending.stop() # just make sure ending won't loop
 
 #if we want to play full song from full file
 func play_full_song(song_idx:int) -> void:
 	var song_name = _song_indexes[str(song_idx)]
 	var _song_data = _get_song_data(song_name)
 	var _full_song_file = _random_file(Array(_song_data['song_full']))
-	$music_full.stream = load(_full_song_file)
+	music_full.stream = load(_full_song_file)
 	var _ending_file_path = _random_file(Array(_song_data['song_endings']))
-	$music_ending.stream = load(_ending_file_path)
-	$music_full.play()
+	music_ending.stream = load(_ending_file_path)
+	music_full.play()
 
 
 func _on_music_full_finished() -> void:
 	pass # Replace with function body.
 
 func stop_song() -> void:
-	$music_middle.stop()
-	$music_intro.stop()
-	$music_ending.stop()
-	$music_full.stop()
+	music_middle.stop()
+	music_intro.stop()
+	music_ending.stop()
+	music_full.stop()
 
 func pause_song(paused:bool=true) -> void:
-	$music_middle.stream_paused = paused
-	$music_intro.stream_paused = paused
-	$music_ending.stream_paused = paused
-	$music_full.stream_paused = paused
+	music_middle.stream_paused = paused
+	music_intro.stream_paused = paused
+	music_ending.stream_paused = paused
+	music_full.stream_paused = paused
 
 func stop_sfx() -> void:
-	$sfx.stop()
+	sfx.stop()
 
 func pause_sfx(paused:bool=true) -> void:
-	$sfx.stream_paused = paused
+	sfx.stream_paused = paused
 func play_sfx(sfx_idx:int) -> void:
 	var sfx_name = _sfx_indexes[str(sfx_idx)]
 	var _sfx_data = _get_sfx_data(sfx_name)
-	$sfx.stream = load(_random_file(_sfx_data['sfx_file']))
-	$sfx.play()
+	sfx.stream = load(_random_file(_sfx_data['sfx_file']))
+	sfx.play()
 #forces to play song ending
 func play_song_ending() -> void:
-	$music_full.stop()
+	music_full.stop()
 	_on_music_middle_finished()
 
 
